6b7fe75579d6b493d580d78b059dc3b1104b7c00
donald93 <blargalicious@gmail.com>
2016-01-03 19:37:59 -0700

    Empty main activity created

---
+++ Bitcrement/app/src/main/java/mcpartland/donald/bitcrement/MainCounter.java
@@ -0,0 +1,18 @@
+package mcpartland.donald.bitcrement;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainCounter extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_counter);
+    }
+
+}
