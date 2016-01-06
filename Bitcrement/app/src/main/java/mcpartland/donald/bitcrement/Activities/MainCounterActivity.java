b5bf52eb44e5951d007988e519d146d813af6e6f
donald93 <blargalicious@gmail.com>
2016-01-05 19:24:33 -0700

    OnClick for button is working

--- Bitcrement/app/src/main/java/mcpartland/donald/bitcrement/Activities/MainCounterActivity.java
+++ Bitcrement/app/src/main/java/mcpartland/donald/bitcrement/Activities/MainCounterActivity.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.inject.Inject;
+
+import mcpartland.donald.bitcrement.Controllers.BitcrementController;
 import mcpartland.donald.bitcrement.R;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
@@ -15,8 +19,14 @@ import roboguice.inject.InjectView;
 public class MainCounterActivity extends RoboActivity
 {
 
-    @InjectView(R.id.mainCounterText) TextView mainCounterText;
-    @InjectView(R.id.mainCounterButton) TextView mainCounterButton;
+    @InjectView(R.id.mainCounterText)
+    TextView mainCounterText;
+
+    @InjectView(R.id.mainCounterButton)
+    TextView mainCounterButton;
+
+    @Inject
+    BitcrementController bitcrementController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,6 +34,11 @@ public class MainCounterActivity extends RoboActivity
         super.onCreate(savedInstanceState);
 
 
+    }
+
+    public void MainCounterButtonOnClick(View view)
+    {
+        mainCounterText.setText(bitcrementController.toString());
     }
 
 }
---
+++ Bitcrement/app/src/main/java/mcpartland/donald/bitcrement/Controllers/BitcrementController.java
@@ -0,0 +1,9 @@
+package mcpartland.donald.bitcrement.Controllers;
+
+/**
+ * Created by Donald on 1/5/16.
+ */
+public class BitcrementController
+{
+
+}
